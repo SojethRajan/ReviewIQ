4a826cf98b3e8c808557d0248431a1e522e20a6e
SojethRajan <sojetha@gmail.com>
2026-06-02 00:31:57 +0530

    downgraded dotnet version from 9 ro 8

--- src/ReviewIQ.Gateway/Program.cs
+++ src/ReviewIQ.Gateway/Program.cs
@@ -13,15 +13,8 @@ if (string.IsNullOrWhiteSpace(keyVaultUrlString))
 
 // Add services
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.MapControllers();
 
