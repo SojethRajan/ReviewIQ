cfcf4f06227a7c750b53bb5eb96d88d97c174224
SojethRajan <sojetha@gmail.com>
2026-06-13 15:30:04 +0530

    testing-003

---
+++ src/ReviewIQ.AI/TestBadClass.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewIQ.AI
+{
+    public class TestBadClass
+    {
+        private string connectionString = "Server=prod-db;Password=Admin123;";
+
+        public void ProcessOrder(int id)
+        {
+            var conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            string query = "SELECT * FROM Orders WHERE Id = " + id;
+            var cmd = new SqlCommand(query, conn);
+            var result = cmd.ExecuteReader();
+
+            if (result != null)
+            {
+                // TODO: fix this later
+                int x = 0;
+                var data = result;
+            }
+        }
+    }
+}
